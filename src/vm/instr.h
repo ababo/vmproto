d40a92759f6f0ca2faa7d188b870a0abfac28f7b
Semion Prikhodko <semion.ababo@gmail.com>
2011-08-01 22:56:23 +0300

    added header tracking in Makefiles; declared instructions needed for factorial test

--- src/vm/instr.h
+++ src/vm/instr.h
@@ -4,30 +4,61 @@
 namespace Ant {
   namespace VM {
 
-#define MAX_INSTR_SIZE 10
-
     typedef uint16_t RegId;
 
-    enum InstrType {
-      INSTR_ILL = 0,
-      INSTR_MOV8L
+    enum OpCode {
+      OPCODE_ILL = 0,
+      OPCODE_MOVM8
     };
- 
+
+#define MAX_INSTR_SIZE 11
+
     class Instr {
     public:
-      virtual size_t size() = 0;
+      OpCode opcode() const {
+        return static_cast<OpCode>(op); }
+
+      virtual size_t size() const = 0;
 
     protected:
-      uint8_t type;
+      uint8_t op;
       uint8_t data[MAX_INSTR_SIZE - 1];
     };
 
-    class MOV8LInstr {
+    class MOVM8Instr : public Instr {
     public:
-      uint16_t from();
-      uint16_t to();
-      void setFrom(RegId from);
-      void setTo(RegId to);
+      MOVM8Instr(uint64_t val, RegId to);
+
+      uint64_t val() const;
+      RegId to() const;
+    };
+
+    class MULInstr : public Instr {
+    public:
+      MULInstr(RegId factor1, RegId factor2, RegId product);
+
+      RegId factor1() const;
+      RegId factor2() const;
+      RegId product() const;
+    };
+
+    class DECInstr : public Instr {
+    public:
+      DECInstr(RegId it);
+
+      RegId it() const;
+    };
+
+    class JZInstr : public Instr {
+    public:
+      JZInstr(int16_t offset);
+
+      int16_t offset() const;
+    };
+
+    class RETInstr : public Instr {
+    public:
+      RETInstr();
     };
 
   }
