d3089289c0323d9b7b85a891a6361c67f8419a61
Semion Prikhodko <semion.ababo@gmail.com>
2011-09-15 00:04:51 +0300

    Temporarily broken

--- src/vm/test/modules.cpp
+++ src/vm/test/modules.cpp
@@ -8,22 +8,34 @@ namespace Ant {
       void createFactorialModule(Module &module) {
         ModuleBuilder builder;
 
+        // void fact(unsigned int *io) {
+        //   if(*io)
+        //     goto l1;
+        //   *io = 1;
+        //   return;
+        // l1:
+        //   {
+        //     unsigned int pr = 1;
+        //   l2:
+        //     pr = *io * pr;
+        //     if(--*io)
+        //       goto l2;
+        //     *io = pr;
+        //   }
+        // }
         VarTypeId vtype = builder.addVarType(8);
-
         RegId io = builder.addReg(vtype);
-        RegId pr = builder.addReg(vtype);
-
         ProcTypeId ptype = builder.addProcType(0, io);
-
         ProcId proc = builder.addProc(PFLAG_EXTERNAL, ptype);
-        builder.addProcInstr(proc, JNZInstr(io, 3));
-        builder.addProcInstr(proc, CPI8Instr(1, io));
-        builder.addProcInstr(proc, RETInstr());
-        builder.addProcInstr(proc, ALSInstr(pr));
+        builder.addProcInstr(proc, JNZInstr(io, 3)); //--|
+        builder.addProcInstr(proc, CPI8Instr(1, io)); // |
+        builder.addProcInstr(proc, RETInstr()); //       |
+        RegId pr = builder.addReg(vtype); //             |
+        builder.addProcInstr(proc, ALSInstr(pr)); //<----|
         builder.addProcInstr(proc, CPI8Instr(1, pr));
-        builder.addProcInstr(proc, MULInstr(io, pr, pr));
-        builder.addProcInstr(proc, DECInstr(io));
-        builder.addProcInstr(proc, JNZInstr(io, -2));
+        builder.addProcInstr(proc, MULInstr(io, pr, pr)); //<-|
+        builder.addProcInstr(proc, DECInstr(io)); //          |
+        builder.addProcInstr(proc, JNZInstr(io, -2)); //------|
         builder.addProcInstr(proc, CPBInstr(pr, io));
         builder.addProcInstr(proc, FRSInstr());
         builder.addProcInstr(proc, RETInstr());
@@ -34,45 +46,83 @@ namespace Ant {
       void createQSortModule(Module &module) {
         ModuleBuilder builder;
 
+        // struct ioType { int l, h, *a; };
         VarTypeId wordType = builder.addVarType(8);
         VarTypeId ioType = builder.addVarType(16);
         builder.addVarTypeVRef(ioType, wordType, 0);
 
+        // void part(struct ioType *io) {
+        //   int l = io->l, h = io->h, *a = io->a, al, ah = a[h];
+        // l1:
+        //   al = a[l];
+        //   if(al > ah)
+        //     goto l2;
+        //   { int h = a[io->l]; a[io->l] = al, a[l] = h, ++io->l; }
+        // l2:
+        //   if(!(++l > h))
+        //     goto l1;
+        //   --io->l;
+        // };
         RegId io = builder.addReg(ioType);
-        RegId m = builder.addReg(wordType, 0);
-        RegId b = builder.addReg(wordType);
-        RegId j = builder.addReg(wordType);
-        RegId mb = builder.addReg(wordType);
-        RegId mj = builder.addReg(wordType);
-        RegId ma = builder.addReg(wordType);
-
         ProcTypeId ptype = builder.addProcType(0, io);
-
         ProcId part = builder.addProc(0, ptype);
-        builder.addProcInstr(part, ALSRInstr(m));
-        builder.addProcInstr(part, ALSInstr(b));
-        builder.addProcInstr(part, ALSInstr(j));
-        builder.addProcInstr(part, ALSInstr(mb));
-        builder.addProcInstr(part, ALSInstr(t1));
-        builder.addProcInstr(part, ALSInstr(t2));
-        builder.addProcInstr(part, LDRInstr(io, 0, m));
-        builder.addProcInstr(part, LDBInstr(io, 8, b));
-        builder.addProcInstr(part, CPBInstr(io, j));
-        builder.addProcInstr(part, LDEInstr(m, b, mb));
-        builder.addProcInstr(part, LDEInstr(m, j, mj));// <----
-        builder.addProcInstr(part, JGInstr(mj, mb, 5)); //--- |
-        builder.addProcInstr(part, LDEInstr(m, io, ma)); // | |
-        builder.addProcInstr(part, STEInstr(mj, m, io)); // | |
-        builder.addProcInstr(part, STEInstr(ma, m, j)); //  | |
+        RegId l = builder.addReg(wordType);
+        builder.addProcInstr(part, ALSInstr(l));
+        builder.addProcInstr(part, CPBInstr(io, l));
+        RegId h = builder.addReg(wordType);
+        builder.addProcInstr(part, ALSInstr(h));
+        builder.addProcInstr(part, LDBInstr(io, 8, h));
+        RegId a = builder.addReg(wordType, 0);
+        builder.addProcInstr(part, ALSRInstr(a));
+        builder.addProcInstr(part, LDRInstr(io, 0, a));
+        RegId al = builder.addReg(wordType);
+        builder.addProcInstr(part, ALSInstr(al));
+        RegId ah = builder.addReg(wordType);
+        builder.addProcInstr(part, ALSInstr(ah));
+        builder.addProcInstr(part, LDEInstr(a, h, ah));
+        builder.addProcInstr(part, LDEInstr(a, l, al)); // <--|
+        builder.addProcInstr(part, JGInstr(al, ah, 7)); // -| |
+        builder.addProcInstr(part, ALSInstr(h)); //         | |
+        builder.addProcInstr(part, LDEInstr(a, io, h)); //  | |
+        builder.addProcInstr(part, STEInstr(al, a, io)); // | |
+        builder.addProcInstr(part, STEInstr(h, a, l)); //   | |
         builder.addProcInstr(part, INCInstr(io)); //        | |
-        builder.addProcInstr(part, INCInstr(j)); //<--------- |
-        builder.addProcInstr(part, JNGInstr(j, b, -8)); //-----
-        builder.addProcInstr(part, FRSNInstr(6));
+        builder.addProcInstr(part, FRSInstr()); //          | |
+        builder.addProcInstr(part, INCInstr(l)); // <-------| |
+        builder.addProcInstr(part, JNGInstr(l, h, -9)); // ---|
         builder.addProcInstr(part, DECInstr(io));
+        builder.addProcInstr(part, FRSNInstr(5));
         builder.addProcInstr(part, RETInstr());
 
+        // void qsort(struct ioType *io) {
+        //   int l = io->l, h = io->h;
+        //   if(h > l)
+        //     goto l1;
+        //   return;
+        // l1:
+        //   part(io);
+        //   h = io->l++;
+        //   qsort(io);
+        //   io->l = l, io->h = --h;
+        //   qsort(io);
+        // }
         ProcId qsort = builder.addProc(PFLAG_EXTERNAL, ptype);
-
+        builder.addProcInstr(qsort, ALSInstr(l));
+        builder.addProcInstr(qsort, CPBInstr(io, l));
+        builder.addProcInstr(qsort, ALSInstr(h));
+        builder.addProcInstr(qsort, LDBInstr(io, 8, h));
+        builder.addProcInstr(qsort, JGInstr(h, l, 2)); // -|
+        builder.addProcInstr(qsort, RETInstr()); //        |
+        builder.addProcInstr(qsort, CALLInstr(part)); // <-|
+        builder.addProcInstr(qsort, CPBInstr(io, h));
+        builder.addProcInstr(qsort, INCInstr(io));
+        builder.addProcInstr(qsort, CALLInstr(qsort));
+        builder.addProcInstr(qsort, CPBInstr(l, io));
+        builder.addProcInstr(qsort, DECInstr(h));
+        builder.addProcInstr(qsort, STBInstr(h, io, 8));
+        builder.addProcInstr(qsort, CALLInstr(qsort));
+        builder.addProcInstr(part, FRSNInstr(2));
+        builder.addProcInstr(qsort, RETInstr());
       }
 
     }
